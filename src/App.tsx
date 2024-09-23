f33b036b3b45a235c0843c6bd744795234c48c17
gzavertyaev-dev <gzavertyaev.dev@gmail.com>
2024-09-23 22:43:55 +0200

    Add lesson 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+// HOMEWORKS
+
+// LESSONS
+import Lesson_06 from "lessons/Lesson_06/Lesson_06";
+
+//CONSULTATIONS
+
 import "./App.css";
 
 function App() {
-  return <div className="App">React Typescript App</div>;
+  return (
+    <div className="App">
+      {/* LESSONS */}
+      <Lesson_06 />
+    </div>
+  );
 }
 
 export default App;

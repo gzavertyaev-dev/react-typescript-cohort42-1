bae381a20718bd1dee375646b757015e6aa5a73f
gzavertyaev-dev <gzavertyaev.dev@gmail.com>
2024-09-25 22:23:14 +0200

    Add lesson 07

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 // HOMEWORKS
+import Homework_06 from "homeworks/Homework_06/Homework_06";
 
 // LESSONS
 import Lesson_06 from "lessons/Lesson_06/Lesson_06";
+import Lesson_07 from "lessons/Lesson_07/Lesson_07";
 
 //CONSULTATIONS
 
@@ -11,7 +13,9 @@ function App() {
   return (
     <div className="App">
       {/* LESSONS */}
-      <Lesson_06 />
+      {/* <Lesson_06 /> */}
+      {/* HOMEWORKS */}
+      <Lesson_07 />
     </div>
   );
 }

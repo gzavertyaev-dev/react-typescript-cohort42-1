ec0805c070f1ac08ee1bc4bae6eb79c133efd753
gzavertyaev-dev <gzavertyaev.dev@gmail.com>
2024-10-07 22:37:45 +0200

    Add lesson 11

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,11 +1,59 @@
+import { useFormik } from "formik";
+import * as Yup from "yup";
+// import { useState } from "react";
+
 import Button from "components/Button/Button";
 import Input from "components/Input/Input";
 
 import { InputsContainer, LoginFormContainer, Title } from "./styles";
 
 function LoginForm() {
+  // Управление элементами формы через state
+  // const [email, setEmail] = useState<string>("");
+  // const [password, setPassword] = useState<string>("");
+  // console.log(email);
+  // console.log(password);
+
+  // const onEmailChange = (event: any) => {
+  //   setEmail(event.target.value);
+  // };
+
+  // const onPasswordChange = (event: any) => {
+  //   setPassword(event.target.value);
+  // };
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .required("Email field is required")
+      .email("Invalid Email format")
+      .min(7, "The minimum email length is 7")
+      .max(30, "The max email length is 30"),
+    password: Yup.string()
+      .required("Password is required")
+      .min(5, "The minimum password length is 5")
+      .max(30, "The max password length is 30"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: validationSchema,
+    validateOnMount: false,
+    validateOnChange: false,
+    onSubmit: (values, helpers) => {
+      // Тут прописываем логику по рабоате с данными, которые пользователь ввел в элементы формы
+      // Например: отправка данных на сервер
+      console.log("Submit works");
+      console.log(values);
+      console.log(helpers);
+      // helpers.resetForm();
+    },
+  });
+
   return (
-    <LoginFormContainer>
+    <LoginFormContainer onSubmit={formik.handleSubmit}>
       <Title>Login form</Title>
       <InputsContainer>
         <Input
@@ -13,6 +61,9 @@ function LoginForm() {
           label="Email"
           placeholder="Enter your email"
           name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+          error={formik.errors.email}
         />
         <Input
           id="login-password"
@@ -20,9 +71,18 @@ function LoginForm() {
           placeholder="Enter your password"
           name="password"
           type="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          error={formik.errors.password}
         />
       </InputsContainer>
-      <Button onClick={() => {}} type="submit" name="Login" />
+      <Button type="submit" name="Login" />
+      {/* <Button
+        onClick={() => {
+          formik.resetForm();
+        }}
+        name="Reset"
+      /> */}
     </LoginFormContainer>
   );
 }
